ba2116bfdbda94d0cc36923f9a23c9e1f0522f58
Márcio Rainer Viçoso <49106790+MARCIORAINER@users.noreply.github.com>
2021-08-18 23:16:54 -0300

    Add files via upload

---
+++ SistemaVendas/SistemaVendas/DAL/userDAL.cs
@@ -0,0 +1,182 @@
+﻿using SistemaVendas.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVendas.DAL
+{
+    class userDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        #region selecionar dados do banco de dados
+        public DataTable Select()
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                String sql = "Select * from tbl_usuarios";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //ABRIR BD, METODO
+                conn.Open();
+                //FILL, TRAZ OS DADOS NO DT
+                adapter.Fill(dt);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                //FECHANDO A CONEXÃO
+                conn.Close();
+            }
+            //FECHAR O RETURN DO DATA TABLE
+            return dt;
+        }
+
+        #endregion
+        #region Inserindo dados no banco de dados
+        public bool Insert(userBLL u)
+        {
+            bool isSucesso = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                String incluisql = "INSERT INTO tbl_usuarios(nome,s_nome,email,usuario,senha,contato,endereco,sexo,usuario_tipo,add_data,add_por) VALUES (@nome,@s_nome,@email,@usuario,@senha,@contato,@endereco,@sexo,@usuario_tipo,@add_data,@add_por)";
+               //PASSAR A STRING DE DADOS E A CONEXÃO
+                SqlCommand cmd = new SqlCommand(incluisql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", u.nome);
+                cmd.Parameters.AddWithValue("@s_nome", u.s_nome);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@usuario", u.usuario);
+                cmd.Parameters.AddWithValue("@senha", u.senha);
+                cmd.Parameters.AddWithValue("@contato", u.contato);
+                cmd.Parameters.AddWithValue("@endereco", u.endereco);
+                cmd.Parameters.AddWithValue("@sexo", u.sexo);
+                cmd.Parameters.AddWithValue("@usuario_tipo", u.usuario_tipo);
+                cmd.Parameters.AddWithValue("@add_data", u.add_data);
+                cmd.Parameters.AddWithValue("@add_por", u.add_por);
+                //ABRI A CONEXÃO
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                //VERIFICAÇÃO DA CONSULTA(QUERY)
+                if (rows > 0)
+                {
+                    isSucesso = true;
+                }
+                else
+                {
+                    isSucesso = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //FECHA CONEXÃO
+                conn.Close();
+            }
+            //RETORNAR
+            return isSucesso;
+
+        }
+        #endregion
+        #region Atualizar dados do banco de dados
+        public bool Update(userBLL u)
+        {
+            bool isSucesso = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                String sql = "UPDATE tbl_usuarios SET nome=@nome,s_nome=@s_nome,email=@email,usuario=@usuario,senha=@senha,contato=@contato,endereco,sexo=@sexo,usuario_tipo=@usuario_tipo,add_data=@add_data,add_por=@add_por WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", u.nome);
+                cmd.Parameters.AddWithValue("@s_nome", u.s_nome);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@usuario", u.usuario);
+                cmd.Parameters.AddWithValue("@senha", u.senha);
+                cmd.Parameters.AddWithValue("@contato", u.contato);
+                cmd.Parameters.AddWithValue("@endereco", u.endereco);
+                cmd.Parameters.AddWithValue("@sexo", u.sexo);
+                cmd.Parameters.AddWithValue("@usuario_tipo", u.usuario_tipo);
+                cmd.Parameters.AddWithValue("@add_data", u.add_data);
+                cmd.Parameters.AddWithValue("@add_por", u.add_por);
+                cmd.Parameters.AddWithValue("@id", u.id);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucesso = true;
+                }
+                else
+                {
+                    isSucesso = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSucesso;
+
+        }
+        #endregion
+        #region Deletar dados do banco de dados
+        public bool Delete(userBLL u)
+        {
+            bool isSucesso = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                String excluisql = "DELETE FROM tbl_usuarios  WHERE id=@id";
+                SqlCommand cmd = new SqlCommand(excluisql, conn);
+
+                cmd.Parameters.AddWithValue("@id", u.id);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucesso = true;
+                }
+                else
+                {
+                    isSucesso = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSucesso;
+
+        }
+        #endregion
+    }
+}
---
+++ SistemaVendas/SistemaVendas/UI/frmUsuarios.cs
@@ -0,0 +1,159 @@
+﻿using SistemaVendas.BLL;
+using SistemaVendas.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVendas.UI
+{
+    public partial class frmUsuarios : Form
+    {
+        public frmUsuarios()
+        {
+            InitializeComponent();
+        }
+        userBLL u = new userBLL();
+        userDAL dal = new userDAL();
+
+
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+
+            u.nome = txtnome.Text;
+            u.s_nome = txtsnome.Text;
+            u.email = txtemail.Text;
+            u.usuario = txtusuario.Text;
+            u.senha = txtsenha.Text;
+            u.contato = txtcontato.Text;
+            u.endereco = txtendereco.Text;
+            u.sexo = cmbsexo.Text;
+            u.usuario_tipo = cmbtipousuario.Text;
+            u.add_data = DateTime.Now;
+            u.add_por = 1;
+
+            bool success = dal.Insert(u);
+            if (success == true)
+            {
+                MessageBox.Show("CADASTRO REALIZADO COM SUCESSO");
+                //CHAMA O MÉTODO LIMPAR OS CAMPOS
+                Limpar();
+            }
+            else
+            {
+                MessageBox.Show("NAO FOI POSSIVEL REALIZAR ESTE CADASTRO");
+            }
+
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            // BUSCA OS DADOS NO BANCO DE DADOS
+
+            DataTable dt = dal.Select();
+            dvgusuario.DataSource = dt;
+
+
+        }
+
+        //MÉTODO PARA LIMPAR OS CAMPOS DO FORMULÁRIO CADASTRO DE USUÁRIOS
+        private void Limpar()
+        {
+            txtid.Text ="";
+            txtnome.Text ="";
+            txtsnome.Text ="";
+            txtemail.Text ="";
+            txtusuario.Text ="";
+            txtsenha.Text ="";
+            txtcontato.Text ="";
+            txtendereco.Text ="";
+            cmbsexo.Text ="";
+            cmbtipousuario.Text ="";
+            
+        }
+
+        private void cmbtipousuario_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dvgusuario_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            
+            int rowIndex = e.RowIndex;
+            //CAMPOS A SEREM CARREGADOS
+            txtid.Text = dvgusuario.Rows[rowIndex].Cells[0].Value.ToString();
+            txtnome.Text = dvgusuario.Rows[rowIndex].Cells[1].Value.ToString();
+            txtsnome.Text = dvgusuario.Rows[rowIndex].Cells[2].Value.ToString();
+            txtemail.Text = dvgusuario.Rows[rowIndex].Cells[3].Value.ToString();
+            txtusuario.Text = dvgusuario.Rows[rowIndex].Cells[4].Value.ToString();
+            txtsenha.Text = dvgusuario.Rows[rowIndex].Cells[5].Value.ToString();
+            txtcontato.Text = dvgusuario.Rows[rowIndex].Cells[6].Value.ToString();
+            txtendereco.Text = dvgusuario.Rows[rowIndex].Cells[7].Value.ToString();
+            cmbsexo.Text = dvgusuario.Rows[rowIndex].Cells[8].Value.ToString();
+            cmbtipousuario.Text = dvgusuario.Rows[rowIndex].Cells[9].Value.ToString();
+        }
+
+        private void btnatualizar_Click(object sender, EventArgs e)
+        {
+            //EVENTO BOTÃO ATUALIZAR , FIZ CONVERSAO DO ID E RECEBENDO COLUNAS OS DADOS DO BD
+
+            u.id = Convert.ToInt32(txtid.Text);
+
+            u.nome = txtnome.Text;
+            u.s_nome = txtsnome.Text;
+            u.email = txtemail.Text;
+            u.usuario = txtusuario.Text;
+            u.senha = txtsenha.Text;
+            u.contato = txtcontato.Text;
+            u.endereco = txtendereco.Text;
+            u.sexo = cmbsexo.Text;
+            u.usuario_tipo = cmbtipousuario.Text;
+            u.add_data = DateTime.Now;
+            u.add_por = 1;
+
+            bool sucesso = dal.Update(u);
+
+            if (sucesso == true)
+            {
+                MessageBox.Show("Cadastro Atualizado com sucesso!");
+                Limpar();
+            }
+            else 
+            {
+               MessageBox.Show("Não foi possível atualizar este cadastro");
+            }
+            //CARREGA O DATA GRID VIEW ATUALIZADO
+            DataTable dt = dal.Select();
+            dvgusuario.DataSource = dt;
+
+        }
+
+        private void btndeletar_Click(object sender, EventArgs e)
+        {
+            u.id = Convert.ToInt32(txtid.Text);
+
+            bool sucesso = dal.Delete(u);
+
+            if (sucesso == true)
+            {
+                MessageBox.Show("Deletado com sucesso!");
+                Limpar();
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível deletar este cadastro");
+            }
+            //CARREGA O DATA GRID VIEW ATUALIZADO
+            DataTable dt = dal.Select();
+            dvgusuario.DataSource = dt;
+
+        }
+    }
+}
